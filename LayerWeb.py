edad394e29396043bf3b72bce1d771654b03834f
dhfgf <aleksandr.sulyagin@gmail.com>
2022-02-12 15:06:02 +0300

    Починил пересоздание

--- LayerWeb.py
+++ LayerWeb.py
@@ -9,25 +9,23 @@ class LayerInfo:
 
 
 class LayerWeb:
-    class Layer:
-        def __init__(self, func, count):
-            self.func = func
-            self.count = count
-
     def __init__(self, input: list, layers_count: int, *layers: LayerInfo):
         self.input = input
         self.layers_count = layers_count
-        self.layers = layers
-
-    def activate(self):
+        self.neuron_layers = []  # лист листов со сгенерированными нейронами
         input_len = len(self.input)
-        input_prev = copy(self.input)
         for cur_layer in range(self.layers_count):
             neurons = []
+            for i in range(layers[cur_layer].neuron_count):
+                neurons.append(Neuron(layers[cur_layer].func, input_len))
+            input_len = layers[cur_layer].neuron_count
+            self.neuron_layers.append(neurons)
+
+    def activate(self):
+        input_prev = copy(self.input)
+        for cur_layer in range(self.layers_count):
             output = []
-            for i in range(self.layers[cur_layer].neuron_count):
-                neurons.append(Neuron(self.layers[cur_layer].func, input_len))
-                output.append(neurons[i].activate(input_prev))
-            input_len = self.layers[cur_layer].neuron_count
+            for i in range(len(self.neuron_layers[cur_layer])):
+                output.append(self.neuron_layers[cur_layer][i].activate(input_prev))
             input_prev = output
         return input_prev
