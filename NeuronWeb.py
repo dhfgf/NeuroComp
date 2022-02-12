5692c7b5582c7f48dc019f42863a8fe9b6a71090
dhfgf <aleksandr.sulyagin@gmail.com>
2022-02-12 14:31:34 +0300

    Слоистая сеть

--- NeuronWeb.py
+++ NeuronWeb.py
@@ -16,3 +16,35 @@ class FullyConnectedWeb:
                 new_input[j] = self.neurons[j].activate(self.input)
             self.input = new_input
         return self.input
+
+
+class LayerInfo:
+    def __init__(self, func, neuron_count):
+        self.func = func
+        self.neuron_count = neuron_count
+
+
+class LayerWeb:
+    class Layer:
+        def __init__(self, func, count):
+            self.func = func
+            self.count = count
+
+    def __init__(self, input: list, layers_count: int, *layers: LayerInfo):
+        self.input = input
+        self.layers_count = layers_count
+        self.layers = layers
+
+    def activate(self):
+        input_len = len(self.input)
+        input_prev = copy(self.input)
+        for cur_layer in range(self.layers_count):
+            neurons = []
+            output = []
+            for i in range(self.layers[cur_layer].neuron_count):
+                neurons.append(Neuron(self.layers[cur_layer].func, input_len))
+                output.append(neurons[i].activate(input_prev))
+            input_len = self.layers[cur_layer].neuron_count
+            input_prev = output
+        return input_prev
+
--- out.py
+++ out.py
@@ -6,7 +6,11 @@ x = [1, 2, 3, 4, 5]
 a = Neuron(func_1, 5)
 b = Neuron(func_2, 5)
 c = Neuron(func_3, 5)
-
 neurons = [a, a, a, a, a]
 A = FullyConnectedWeb(neurons, 1, x)
-print(A.activate())
+
+l1 = LayerInfo(func_1, 5)
+l2 = LayerInfo(func_1, 4)
+l3 = LayerInfo(func_1, 3)
+B = LayerWeb(x, 3, l1, l2, l3)
+print(B.activate())
